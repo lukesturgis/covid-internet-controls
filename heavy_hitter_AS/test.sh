dc5f6ae9b0ad8c2c2ea949ea72c3c9cce456ea40
sahilgupta221 <sahil15057@iiitd.ac.in>
2020-05-15 21:56:17 -0400

    Fifth push

--- heavy_hitter_AS/test.sh
+++ heavy_hitter_AS/test.sh
@@ -18,6 +18,6 @@ D="${D/'/'/}"
 #echo $C
 #echo $D
 #python3 heavy_hitter_list_of_all_AS_in_country_list.py $i
-python3 heavy_hitter_list_of_all_AS_in_country_list.py $i| grep AS[0-9] > $C ; sed -i 's/Data  : AS//g' $C ; sed '1,100!d' $C > $D; IFS=$'\n' read -d '' -r -a lines < $D ; echo ${lines[*]}
+python3 heavy_hitter_list_of_all_AS_in_country_list.py $i| grep AS[0-9]|grep -v -e '"' > $C ; sed -i 's/Data  : AS//g' $C ; sed '1,100!d' $C > $D; IFS=$'\n' read -d '' -r -a lines < $D ; echo ${lines[*]}
 #pkill python3
 done
