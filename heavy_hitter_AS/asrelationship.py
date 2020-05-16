5e681761b9a0088b8d4c1932c55fb14e3e91ee3b
sahilgupta221 <sahilgupta221@gmail.com>
2020-05-16 17:08:52 -0400

    graph is constructed. searching child is todo

--- heavy_hitter_AS/asrelationship.py
+++ heavy_hitter_AS/asrelationship.py
@@ -1,24 +1,54 @@
-import json # pretty print nested hash
-L = ["Geeks\n", "for\n", "Geeks\n"] 
-#	 print("{}:{}:{}".format(as_array[0],as_array[1],as_array[2]))	
-#        print("Line{}: {}".format(count, line.strip())) # Writing to file   
-# Using for loop 
-count = 0
+ 
+class AdjNode:
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V
+
+    # Add edges
+    def add_edge(self, src, dest):
+        node = AdjNode(dest)
+        node.next = self.graph[src]
+        self.graph[src] = node
+
+#        node = AdjNode(src)
+#        node.next = self.graph[dest]
+#        self.graph[dest] = node
+
+    # Print the graph
+    def print_graph(self):
+        for i in range(self.V):
+            print("Adjacency list of vertex {}\n head".format(i))
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex))
+                temp = temp.next
+            print(" \n")
+
+
 as_array = []
-as_relationship_list = []
-as_chidrens= [[0 for i in range(10000)] for j in range(10000)]
 
 with open("20200501_as_rel.txt") as fp: 
+    V = 400000
+    # Create graph and edges
+    graph = Graph(V)
+
     for line in fp: 
-        count += 1
+
 	as_array = line.split('|')
+
 	if as_array[2] == "-1\n": 
-#		as_array[0], as_array[1] = as_array[1], as_array[0]	
-		as_chidrens[int(as_array[0])][int(as_array[1])] = "YES"
-		as_relationship_list.append(as_array[0:2])
+		graph.add_edge(int(as_array[0]), int(as_array[1]))
 
-print(as_relationship_list[-1])
-print(as_chidrens)
+    graph.print_graph()
 
-with open("mytestfile.txt", "w") as fp: 
-    fp.writelines(L) 
+#ToDo:
+#heavy hitter
+#verfication of 90% internet is covered from heavy_hitter file
+
+
