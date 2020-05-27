eaee9c228fa44f6881d88c3164dd5e430395a39d
Luke Sturgis <lcs2329@rit.edu>
2020-05-27 10:45:06 -0400

    fixes & removing ssl for now

--- workers.py
+++ workers.py
@@ -1,1 +1,13 @@
-workers = []
+workers = [
+    {"location": "Korea", "ip": "103.140.45.134"},
+    {"location": "Shanghai", "ip": "129.21.183.10"},
+    {"location": "Shanghai2", "ip": "111.231.28.232"},
+    {"location": "Beijing", "ip": "49.233.48.214"},
+    {"location": "Guangzhou", "ip": "111.230.200.28"},
+    {"location": "Chengdu", "ip": "139.155.74.236"},
+    {"location": "Chongqing", "ip": "129.28.202.97"},
+    {"location": "Hong_Kong", "ip": "124.156.101.160"},
+    {"location": "South_Africa", "ip": "102.67.140.37"},
+    {"location": "Australia", "ip": " 109.201.143.179"},
+    {"location": "Peru", "ip": "45.7.230.136"},
+]
