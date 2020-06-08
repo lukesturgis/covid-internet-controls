a97d6e2c8ff816a124cd5273c4f35f6e57eda24f
Luke Sturgis <lcs2329@rit.edu>
2020-06-08 10:55:27 -0400

    cleanup and fixes

--- mysql_database/create_tables.sql
+++ mysql_database/create_tables.sql
@@ -1,13 +1,18 @@
 CREATE DATABASE IF NOT EXISTS covid_internet_controls;
 USE covid_internet_controls;
 
-CREATE TABLE IF NOT EXISTS workers (
-    id INT NOT NULL AUTO_INCREMENT,
-    worker_ip VARCHAR(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS countries (
     country_code VARCHAR(2) NOT NULL,
     country_name VARCHAR(64) NOT NULL,
     continent VARCHAR(64),
-    PRIMARY KEY (worker_ip); 
+    PRIMARY KEY (country_code)
+);
+
+CREATE TABLE IF NOT EXISTS workers (
+    worker_ip VARCHAR(32) NOT NULL,
+    country_code VARCHAR(2) NOT NULL,
+    PRIMARY KEY (worker_ip), 
+    FOREIGN KEY (country_code) REFERENCES countries(country_code)
 );
 
 CREATE TABLE IF NOT EXISTS response (
@@ -15,6 +20,7 @@ CREATE TABLE IF NOT EXISTS response (
     success BOOLEAN NOT NULL,
     status_code INT NOT NULL,
     content TEXT NOT NULL
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS request (
@@ -25,6 +31,6 @@ CREATE TABLE IF NOT EXISTS request (
     response_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (worker_ip) REFERENCES workers(worker_ip),
-    FOREIGN KEY (response_id) REFERENCES responses(id)
+    FOREIGN KEY (response_id) REFERENCES response(id)
 );
 
