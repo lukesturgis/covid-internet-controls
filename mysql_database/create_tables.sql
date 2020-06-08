0383ce45639daedc5ebb56c7fe6064c0ec61068e
Luke Sturgis <lcs2329@rit.edu>
2020-06-08 11:27:08 -0400

    update for query workers

--- mysql_database/create_tables.sql
+++ mysql_database/create_tables.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS countries (
 CREATE TABLE IF NOT EXISTS workers (
     worker_ip VARCHAR(32) NOT NULL,
     country_code VARCHAR(2) NOT NULL,
+    city VARCHAR(256),
     PRIMARY KEY (worker_ip), 
     FOREIGN KEY (country_code) REFERENCES countries(country_code)
 );
@@ -19,7 +20,7 @@ CREATE TABLE IF NOT EXISTS response (
     id INT NOT NULL AUTO_INCREMENT,
     success BOOLEAN NOT NULL,
     status_code INT NOT NULL,
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -29,6 +30,7 @@ CREATE TABLE IF NOT EXISTS request (
     domain VARCHAR(256) NOT NULL,
     path VARCHAR(256) NOT NULL,
     response_id INT NOT NULL,
+    protocol VARCHAR(5) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (worker_ip) REFERENCES workers(worker_ip),
     FOREIGN KEY (response_id) REFERENCES response(id)
