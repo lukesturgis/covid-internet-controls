1e6adffeacd0895b830ab41c35adee1f10bb2bd5
sahilgupta221 <sahil15057@iiitd.ac.in>
2020-07-02 12:54:27 -0400

    tested HB1 and HB2 server as VPS with ansible

--- query_worker_multiple_targets.sh
+++ query_worker_multiple_targets.sh
@@ -3,6 +3,6 @@ IFS=$'\n' read -d '' -r -a websites < website_list.txt ; echo ${websites[*]}
 for i in "${websites[@]}"
 do
 echo $i
-sudo python3 query_workers.py -t $i -v
+sudo python3.6 query_workers.py -t $i -v
 sleep 5
 done
--- workers.py
+++ workers.py
@@ -6,6 +6,13 @@ workers = [
         "city": "Rochester",
         "ip": "129.21.183.44",
     },
+    {
+        "country_name": "United_States",
+        "country_code": "US",
+        "continent": "America",
+        "city": "Rochester",
+        "ip": "129.21.183.46",
+    },
 ]
 
 
