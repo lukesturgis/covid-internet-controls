6f15b28e313742fafb2b638744911de43cf60d42
Your Name <you@example.com>
2020-07-04 20:58:44 -0400

    added random wait time in wrapper script to avoid DoS detection

--- query_worker_multiple_targets.sh
+++ query_worker_multiple_targets.sh
@@ -3,6 +3,7 @@ IFS=$'\n' read -d '' -r -a websites < website_list.txt ; echo ${websites[*]}
 for i in "${websites[@]}"
 do
 echo $i
+ranNum=$[RANDOM%240+120]
 sudo python3.6 query_workers.py -t $i -v
-sleep 5
+sleep $ranNum
 done
