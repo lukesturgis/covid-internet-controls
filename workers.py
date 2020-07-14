c3a6e26c0d4e25a6f3a3a63494c8760cd26a49d3
sahilgupta221 <sahilgupta221@gmail.com>
2020-07-14 02:07:07 +0000

    added more VPSes

--- workers.py
+++ workers.py
@@ -37,6 +37,146 @@ workers = [
 ]
 
 
+"""
 
+	Australia_sydney:
+          ansible_host: 139.99.140.84
+          ansible_password: easypass321!
+          ansible_user: root
+        Brazil:
+          ansible_host: 178.63.116.245
+          ansible_password: easypass321!
+          ansible_user: root
+        Brazil_Sao_Paulo:
+          ansible_host: 45.179.88.217
+          ansible_password: easypass321!
+          ansible_user: root
+        Bursa_TR:
+          ansible_host: 185.181.209.228
+          ansible_password: easypass321!
+          ansible_port: 28954
+          ansible_user: root
+        China_Chengdu:
+          ansible_host: 139.155.2.148
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        China_Chongqing:
+          ansible_host: 129.28.157.165
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        China_Guangzhou:
+          ansible_host: 203.195.154.91
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        China_Shanghai:
+          ansible_host: 81.68.74.156
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        China_beijing:
+          ansible_host: 140.143.36.180
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        Egypt:
+          ansible_host: 41.223.52.157
+          ansible_password: easypass321!
+          ansible_user: root
+        Germany:
+          ansible_host: 194.76.226.113
+          ansible_password: easypass321!
+          ansible_user: root
+        Hongkong:
+          ansible_host: 129.226.169.60
+          ansible_password: easypass321!
+          ansible_user: ubuntu
+        India_Noida:
+          ansible_host: 103.129.97.180
+          ansible_password: easypass321!
+          ansible_user: root
+        Italy_Milan:
+          ansible_host: 94.198.97.45
+          ansible_password: easypass321!
+          ansible_user: root
+        Japan_Tokyo:
+          ansible_host: 210.140.10.50
+          ansible_password: easypass321!
+          ansible_user: root
+        LA:
+          ansible_host: 173.232.146.200
+          ansible_password: easypass321!
+          ansible_user: root
+        London:
+          ansible_host: 212.71.252.140
+          ansible_password: easypass321!
+          ansible_user: root
+        Millan:
+          ansible_host: 185.213.20.242
+          ansible_password: easypass321!
+          ansible_user: root
+        New_York:
+          ansible_host: 23.227.178.109
+          ansible_password: easypass321!
+          ansible_user: root
+        Ohio:
+          ansible_host: 18.191.43.171
+          ansible_password: easypass321!
+          ansible_user: root
+        Russia_Moscow:
+          ansible_host: 91.237.249.130
+          ansible_password: easypass321!
+          ansible_user: root
+        Seattle:
+          ansible_host: 23.227.190.135
+          ansible_password: easypass321!
+          ansible_user: root
+        Seoul:
+          ansible_host: 141.164.46.236
+          ansible_password: easypass321!
+          ansible_user: root
+        Siberia_Khabarovsk:
+          ansible_host: 92.223.72.165
+          ansible_password: easypass321!
+          ansible_user: root
+        Singapore:
+          ansible_host: 139.99.31.107
+          ansible_password: easypass321!
+          ansible_user: root
+        SouthAfrica_Johannesburg:
+          ansible_host: 102.130.116.16
+          ansible_password: easypass321!
+          ansible_port: 28954
+          ansible_user: root
+        SouthKorea_seoul:
+          ansible_host: 27.255.75.155
+          ansible_password: easypass321!
+          ansible_user: root
+        Sydney:
+          ansible_host: 172.105.190.189
+          ansible_password: easypass321!
+          ansible_user: root
+        Tokyo:
+          ansible_host: 172.104.79.212
+          ansible_password: easypass321!
+          ansible_user: root
+        Turkey_Istanbul:
+          ansible_host: 152.89.36.213
+          ansible_password: easypass321!
+          ansible_user: root
+        Ukraine_Kyiv:
+          ansible_host: 176.107.188.184
+          ansible_password: easypass321!
+          ansible_user: root
+        United_kingdom_London:
+          ansible_host: 79.141.170.113
+          ansible_password: easypass321!
+          ansible_user: root
+        Virginia:
+          ansible_host: 3.235.150.29
+          ansible_password: easypass321!
+          ansible_user: root
+        Washinton:
+          ansible_host: 38.68.38.170
+          ansible_password: easypass321!
+          ansible_user: root
 
+"""
 
