1cf0519b4496619802c274e30b02067058e09c84
sahilgupta221 <sahilgupta221@gmail.com>
2020-07-16 21:57:04 +0000

    Bug fix: path added to shell scripts to match the crontab enviroment

--- query_worker_multiple_targets.sh
+++ query_worker_multiple_targets.sh
@@ -1,4 +1,7 @@
-IFS=$'\n' read -d '' -r -a websites < website_list.txt ; echo ${websites[*]}
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+IFS=$'\n' read -d '' -r -a websites < /home/sg5414/covid-internet-controls/website_list.txt ; echo ${websites[*]}
 
 for i in "${websites[@]}"
 do
