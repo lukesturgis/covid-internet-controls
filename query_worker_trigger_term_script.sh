67f38ad7adb1c2265554344629f0afef6c452a92
sahilgupta221 <sahilgupta221@gmail.com>
2020-07-16 21:57:04 +0000

    Bug fix: path added to shell scripts to match the crontab enviroment

--- query_worker_trigger_term_script.sh
+++ query_worker_trigger_term_script.sh
@@ -1,4 +1,7 @@
-IFS=$'\n' read -d '' -r -a websites < trigger_terms_website.txt ; echo ${websites[*]}
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+IFS=$'\n' read -d '' -r -a websites < /home/sg5414/covid-internet-controls/trigger_terms_website.txt ; echo ${websites[*]}
 touch sahiltest.txt
 for i in "${websites[@]}"
 do
